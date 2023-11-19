99a5424b93f345cfce7c916b81c137d03484874e
lvalmeida <151419266+lvalmeida@users.noreply.github.com>
2023-11-19 01:31:47 -0300

    Add files via upload

---
+++ projeto1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+    int vec[8];
+    int tam = sizeof(vec)/ sizeof(vec[0]);
+    int temp = 0;
+
+  for (int i = 0; i < tam; i++) {
+    printf("Entre com um inteiro para vetor[8]: ");
+    scanf("%d", &temp);
+    vec[i] = temp;
+  }
+
+    printf("\n\nVetor inicial:\n");
+
+    for (int i = 0; i < tam; i++) {
+        printf("%d ", vec[i]);
+    }
+
+    printf("\n\nVetor depois da ordenação:\n");
+
+    for (int i = 0; i < tam - 1; i++) {
+        for (int j = 0; j < tam - i - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
+                int temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < tam; i++) {
+        printf("%d ", vec[i]);
+    }
+
+    return 0;
+}
